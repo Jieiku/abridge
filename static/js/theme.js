cd71559564732e4f2b4752560ff1b6de6f737a79
Jieiku <106644+Jieiku@users.noreply.github.com>
2023-07-19 13:32:33 -0700

    nojs configurable default switcher theme

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,4 +1,4 @@
 // enforce local storage setting but also fallback to user-agent preferences
-if (localStorage.getItem('theme') === 'light' || (!localStorage.getItem('theme') && !window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-  document.documentElement.classList.add('light');
+if (localStorage.getItem('theme') === 'switch' || (!localStorage.getItem('theme') && !window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  document.documentElement.classList.add('switch');
 }
--- static/js/theme_button.js
+++ static/js/theme_button.js
@@ -1,7 +1,7 @@
 // Set lightmode
 if (document.getElementById('mode')) {
   document.getElementById('mode').addEventListener('click', () => {
-    document.documentElement.classList.toggle('light');
-    localStorage.setItem('theme', document.documentElement.classList.contains('light') ? 'light' : 'dark');
+    document.documentElement.classList.toggle('switch');
+    localStorage.setItem('theme', document.documentElement.classList.contains('switch') ? 'switch' : 'default');
   });
 }
